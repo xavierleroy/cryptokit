8dc4c36252e7bc4fc05e667f71106106cbdf3e40
Xavier Leroy <xavier.leroy@college-de-france.fr>
2019-10-29 18:18:09 +0100

    Don't use "Pervasives." to avoid OCaml 4.09 warning

--- setup.ml
+++ setup.ml
@@ -1772,7 +1772,7 @@ module OASISFeatures = struct
     Map.Make
       (struct
         type t = plugin_kind * name
-        let compare = Pervasives.compare
+        let compare = compare
       end)
 
   module Data =
@@ -3465,7 +3465,7 @@ module BaseEnv = struct
           ([], None)
           (List.sort
              (fun (o1, _) (o2, _) ->
-                Pervasives.compare o2 o1)
+                compare o2 o1)
              lst)
       in
       match res, errors with
