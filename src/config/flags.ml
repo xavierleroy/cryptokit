54e728bcc3584c8e6300ea0ec8b0dd9d082f5d7e
Andrey Mokhov <amokhov@janestreet.com>
2020-08-21 13:03:38 +0100

    Use configurator's [write_sexp]

--- src/config/flags.ml
+++ src/config/flags.ml
@@ -1,21 +1,18 @@
 (* Compute compilation and linking flags *)
 
-let write_sexp file list =
-  let oc = open_out file in
-  let content = List.map (Printf.sprintf "%S") list |> String.concat " " in
-  Printf.fprintf oc "(%s)" content;
-  close_out oc
+module Configurator = Configurator.V1
 
 (* Compile and link a dummy C program with the given flags. *)
 let test ~c_flags ~link_flags =
   let test_program = "int main() { return 0; }" in
-  let c = Configurator.V1.create "cryptokit" in
-  Configurator.V1.c_test c test_program ~c_flags ~link_flags
+  let c = Configurator.create "cryptokit" in
+  Configurator.c_test c test_program ~c_flags ~link_flags
 
 let compute_flags ~os_type ~system ~architecture =
   let zlib = os_type <> "Win32" in
   let hardwaresupport =
-    (architecture = "amd64" || architecture = "i386") && test ~c_flags:["-maes"] ~link_flags:[]
+    (architecture = "amd64" || architecture = "i386")
+    && test ~c_flags:[ "-maes" ] ~link_flags:[]
   in
   let append_if c y x = if c then x @ [ y ] else x in
   let flags =
@@ -30,13 +27,13 @@ let compute_flags ~os_type ~system ~architecture =
     |> append_if (system = "win32" || system = "win64") "advapi32.lib"
     |> append_if (system = "mingw" || system = "mingw64") "-ladvapi32"
   in
-  write_sexp "flags.sexp" flags;
-  write_sexp "library_flags.sexp" library_flags
+  Configurator.Flags.write_sexp "flags.sexp" flags;
+  Configurator.Flags.write_sexp "library_flags.sexp" library_flags
 
 let () =
   match Sys.argv with
-  | [|
-   _; "-os_type"; os_type; "-system"; system; "-architecture"; architecture;
-  |] ->
-      compute_flags ~os_type ~system ~architecture
+  | [| _; "-os_type"; os_type;
+          "-system"; system;
+          "-architecture"; architecture;
+    |] -> compute_flags ~os_type ~system ~architecture
   | _ -> failwith "unexpected command line arguments"
