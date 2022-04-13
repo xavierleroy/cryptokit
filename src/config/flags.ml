51807be5884cf41043ca3c111c854ba36b403f9f
Xavier Leroy <xavier.leroy@college-de-france.fr>
2022-04-13 18:55:50 +0200

    Also test for -mpclmul

--- src/config/flags.ml
+++ src/config/flags.ml
@@ -20,13 +20,14 @@ let () = Configurator.main ~name:"cryptokit" (fun cfg ->
   let hardware_support = match enable_hardware_support with
     | This bool -> bool
     | Auto -> (architecture = "amd64" || architecture = "i386")
-              && test ~cfg ~c_flags:[ "-maes" ] ~link_flags:[]
+              && test ~cfg ~c_flags:[ "-maes"; "-mpclmul" ] ~link_flags:[]
   in
   let append_if c y x = if c then x @ [ y ] else x in
   let flags =
     []
     |> append_if zlib "-DHAVE_ZLIB"
     |> append_if hardware_support "-maes"
+    |> append_if hardware_support "-mpclmul"
   in
   let library_flags =
     []
