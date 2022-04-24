dd36602998d9dec53aa8637e2e066208d3c8ec2d
Doug Patti <douglas.patti@gmail.com>
2022-04-24 12:14:51 -0400

    add hmac_sha384 function (#8)
    Using the existing `HMAC` functor, apply the `Hash.sha384` class to
    create a module and exposed function for creating HMAC hashes with the
    SHA-384 algorithm.
    
    The inputs for these tests were copied from the HMAC-SHA512 tests below
    it. For each test, I made sure that I could produce the expected
    HMAC-SHA512 output using `openssl sha512 -hmac` on the command line, and
    then I switched `sha512` to `sha384`. The resulting output was used as
    the expected value for each of the tests, and they all passed. This
    makes me reasonably confident in its correctness.

--- src/cryptokit.ml
+++ src/cryptokit.ml
@@ -1401,6 +1401,8 @@ module HMAC_SHA1 =
   HMAC(struct class h = Hash.sha1  let blocksize = 64 end)
 module HMAC_SHA256 =
   HMAC(struct class h = Hash.sha256  let blocksize = 64 end)
+module HMAC_SHA384 =
+  HMAC(struct class h = Hash.sha384  let blocksize = 128 end)
 module HMAC_SHA512 =
   HMAC(struct class h = Hash.sha512  let blocksize = 128 end)
 module HMAC_RIPEMD160 = 
@@ -1410,6 +1412,7 @@ module HMAC_MD5 =
 
 let hmac_sha1 key = new HMAC_SHA1.hmac key
 let hmac_sha256 key = new HMAC_SHA256.hmac key
+let hmac_sha384 key = new HMAC_SHA384.hmac key
 let hmac_sha512 key = new HMAC_SHA512.hmac key
 let hmac_ripemd160 key = new HMAC_RIPEMD160.hmac key
 let hmac_md5 key = new HMAC_MD5.hmac key
